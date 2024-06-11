bff65c8417dfe6d35488012d27b5c7801282a746
Obah Sylva <sylvan452@gmail.com>
2024-06-11 16:40:59 +0100

    feat: final fixes for payment integration

--- loader.ts
+++ loader.ts
@@ -4,8 +4,8 @@ export default function myImageLoader({ src, width, quality }: any) {
   if ((src as string).startsWith('http')) {
     return src;
   }
-  return `${process.env.NEXT_PUBLIC_SERVER_URL}/${src}?w=${width}&q=${
-    quality || 75
+  return `${process.env.NEXT_PUBLIC_SERVER_URL}/${src}?w=${width}${
+    quality ? '&q=' + quality : ''
   }`;
   // return `${process.env.NEXT_PUBLIC_SERVER_URL}/${src}?w=${width}&q=${
   //   quality || 75
